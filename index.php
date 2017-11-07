c7517228518cf0dd1e3618973b96792ec958a21f
Taryn <taryn.greer@wpengine.com>
2017-11-07 15:56:15 -0600

    started widget

--- index.php
+++ index.php
@@ -5,3 +5,14 @@ Description: Finds coffee shops near you
 Version: 0.0.1
 Author: Taryn Greer | Jason Stallings
 */
+add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+
+function my_custom_dashboard_widgets() {
+global $wp_meta_boxes;
+
+wp_add_dashboard_widget('custom_help_widget', 'Theme Support', 'custom_dashboard_help');
+}
+
+function custom_dashboard_help() {
+echo '<p>Welcome to Custom Blog Theme! Need help? Contact the developer <a href="mailto:yourusername@gmail.com">here</a>. For WordPress Tutorials visit: <a href="http://www.wpbeginner.com" target="_blank">WPBeginner</a></p>';
+}
