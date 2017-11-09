9f4649a47bd4ae56601a86423e154b6ba54cacd6
Taryn <taryn.greer@wpengine.com>
2017-11-09 15:53:00 -0600

    Styling and started formatting display

--- index.php
+++ index.php
@@ -16,7 +16,25 @@ wp_add_dashboard_widget('wp_coffee', 'WP Coffee', 'wp_coffee_dashboard_widget');
 function wp_coffee_dashboard_widget() {
   $results = file_get_contents('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20local.search%20where%20query=%22coffee%22%20and%20location=%22Austin,%20TX%22%20and%20Rating.AverageRating%3E=3&format=json');
 $parsed_results = json_decode($results, true);
-var_dump($parsed_results['query']['results']['Result'][0]['Title']);
+$shops = $parsed_results['query']['results']['Result'];
+foreach ($shops as $shop) {
+  ?>
+  <span style="font-weight:600;">
+    <a href="<?php echo $shop['BusinessUrl'];?>" target="_blank">
+      <?php echo $shop['Title'];?>
+    </a>
+  </span>
+  <div>
+    Rating: <?php echo $shop['Rating']['AverageRating'];?>
+<br/>
+    Address: <a href="<?php echo $shop['MapUrl'];?>" target="_blank">
+<?php echo $shop['Address'];?>
+</a>
+  </div>
+  <hr/>
+  <?php
+}
+
   ?>
 <p align="center"><b>Welcome to WP Coffee!</b> <br> <small><i>Find coffee shops near you.</small></i></p>
 <p> A map would probably go here. </p>
